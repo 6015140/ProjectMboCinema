7f214e3953eceecb1266947c05a55c3f23bf89e6
JasonHoups <6015140@mborijnland.nl>
2025-01-17 11:42:27 +0100

    films kunnen geedit worden

--- editFilm.php
+++ editFilm.php
@@ -1,12 +1,13 @@
 <?php
 require 'classes/film.php';
 $film = new Film();
-if(isset($_POST['submit'])){
-    //$film->editFilm($_GET['id']);
+if(isset($_POST['change'])){
+    $film->bewerkfilms($_GET['id']);
+    
     header("Location: films.php");
 }
-$film = $film->getFilmById($_GET['id']);
 
+$currFilm = $film->getFilmById($_GET['id']);
 
 ?>
 <!DOCTYPE html>
@@ -14,21 +15,21 @@ $film = $film->getFilmById($_GET['id']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Voeg een Film toe</title>
+    <title>Edit een film</title>
 </head>
 <?php
 include "Header.php";
 ?>
 <body>
-    <h1>Voeg een nieuwe Film toe</h1>
-    <form action="addfilm.php" method="post">
-        <input type="text" name="titel" value="<?php echo $film['titel'];?>"placeholder="Title" required>
-        <textarea name="omschrijving" placeholder="Description" required><?php echo $film['omschrijving'];?></textarea>
-        <input type="text" name="release_datum" value="<?php echo $film['release_datum'];?> "placeholder="Release Date" required>
-        <input type="text" name="genre" value="<?php echo $film['genre'];?>"placeholder="Genre" required>
-        <input type="number" name="duur" value="<?php echo $film['duur'];?>" placeholder="Duration (minuten)" required>
-        <input type="url" name="poster_url" value="<?php echo $film['poster_url'];?>" placeholder="Poster URL" required>
-        <button name="submit" ="submit">editFilm</button>
+    <h1>edit film informatie</h1>
+    <form action="" method="post">
+        <input type="text" name="titel" value="<?php echo $currFilm['titel'];?>"placeholder="Title" required>
+        <textarea name="omschrijving" placeholder="Description" required><?php echo $currFilm['omschrijving'];?></textarea>
+        <input type="text" name="release_datum" value="<?php echo $currFilm['release_datum'];?> "placeholder="Release Date" required>
+        <input type="text" name="genre" value="<?php echo $currFilm['genre'];?>"placeholder="Genre" required>
+        <input type="number" name="duur" value="<?php echo $currFilm['duur'];?>" placeholder="Duration (minuten)" required>
+        <input type="url" name="poster_url" value="<?php echo $currFilm['poster_url'];?>" placeholder="Poster URL" required>
+        <button name="change" type="submit">editFilm</button>
     </form>
 
 </body>
